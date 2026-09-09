3b9b6b263aa176fcd0f62167c2ca616371a130f8
EduHoff <ehcs.business@proton.me>
2026-09-09 12:02:25 -0300

    feat(nary_tree): implementa nary_tree_add_child

--- src/ds/generic_tree.c
+++ src/ds/generic_tree.c
@@ -24,7 +24,7 @@ void nary_tree_free(NaryTreeNode* root) {
 static void nary_tree_traverse_preorder_aux(NaryTreeNode* node, NaryTreeFn fn, void* args, int current_depth) {
     if (node == NULL) return;
 
-    fn(node, current_depth, args); // Modificado: repassa current_depth
+    fn(node, current_depth, args);
     nary_tree_traverse_preorder_aux(node->first_child, fn, args, current_depth + 1);
     nary_tree_traverse_preorder_aux(node->next_sibling, fn, args, current_depth);
 }
@@ -46,18 +46,34 @@ void nary_tree_traverse_postorder(NaryTreeNode* root, NaryTreeFn fn, void* args)
 }
 
 NaryTreeNode* nary_tree_find(NaryTreeNode* root, const void* target_data, int (*compare)(const void*, const void*)) {
-    if (root == NULL) {
-        return NULL;
-    }
+    if (root == NULL) return NULL;
 
     if (compare(root->data, target_data) == 0) {
         return root;
     }
 
     NaryTreeNode* found = nary_tree_find(root->first_child, target_data, compare);
-    if (found != NULL) {
-        return found;
-    }
+    if (found != NULL) return found;
 
     return nary_tree_find(root->next_sibling, target_data, compare);
+}
+
+int nary_tree_add_child(NaryTreeNode* parent_node, void* data, int weight) {
+    if (parent_node == NULL) return 0;
+
+    NaryTreeNode* new_node = nary_tree_create_node(data, weight);
+    if (new_node == NULL) return 0;
+
+    if (parent_node->first_child == NULL) {
+        parent_node->first_child = new_node;
+        return 1;
+    }
+
+    NaryTreeNode* aux = parent_node->first_child;
+    while (aux->next_sibling != NULL) {
+        aux = aux->next_sibling;
+    }
+
+    aux->next_sibling = new_node;
+    return 1;
 }
--- src/main.c
+++ src/main.c
@@ -5,15 +5,10 @@ int main(void) {
     clear_screen();
 
     NaryTreeNode* diretor = nary_tree_create_node("Diretor Geral", 1);
-    NaryTreeNode* ger_ti = nary_tree_create_node("Gerente de TI", 1);
-    NaryTreeNode* ger_vendas = nary_tree_create_node("Gerente de Vendas", 1);
-    NaryTreeNode* dev = nary_tree_create_node("Desenvolvedor", 1);
-    NaryTreeNode* analista = nary_tree_create_node("Analista de Vendas", 1);
-
-    diretor->first_child = ger_ti;
-    ger_ti->next_sibling = ger_vendas;
-    ger_ti->first_child = dev;
-    ger_vendas->first_child = analista;
+    nary_tree_add_child(diretor, "Gerente de TI", 1);
+    nary_tree_add_child(diretor, "Gerente de Vendas", 1);
+    nary_tree_add_child(diretor->first_child, "Desenvolvedor", 1);
+    nary_tree_add_child(diretor->first_child->next_sibling, "Analista de Vendas", 1);
 
     nary_tree_print_organogram(diretor);
     nary_tree_free(diretor);
